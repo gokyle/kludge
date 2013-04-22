a9386c6b143f1fb1fb16bda4a7a16998748a78ea
Kyle Isom <coder@kyleisom.net>
2013-04-21 22:30:32 -0600

    Add get support.

--- kludge-backend/pool.go
+++ kludge-backend/pool.go
@@ -1,5 +1,6 @@
 package main
 
+import "github.com/jmhodges/levigo"
 import "github.com/gokyle/kludge/common"
 import "log"
 
@@ -19,8 +20,28 @@ func requestHandler(id int) {
 			log.Printf("worker %d returns", id)
 			return
 		}
-		switch req.Op.OpCode {
 
+		log.Printf("worker %d handling %s request", id,
+			req.OpName())
+		switch req.Op.OpCode {
+		case common.OpGet:
+                        resp := store_get(req.Op)
 		}
 	}
+}
+
+func store_get(op common.Request) (resp common.Response) {
+	ropts := levigo.NewReadOptions()
+        ropts.SetVerifyChecksums(true)
+
+        data, err := ldb.Get(ropts, op.Key)
+        if err != nil {
+                log.Printf("error handling get from worker %d: %s",
+                        id, err.Error())
+        } else {
+                log.Printf("worker %d successfully completes GET", id)
+                resp = new(common.Response)
+                resp.Body = data
+        }
+        return
 }
