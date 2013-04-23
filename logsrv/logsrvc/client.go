3b0e1714aeb3da4ace5ed4f456a1649550cd4bda
Kyle Isom <coder@kyleisom.net>
2013-04-23 16:56:27 -0600

    Add logsrv client library.

---
+++ logsrv/logsrvc/client.go
@@ -0,0 +1,59 @@
+// Package logsrvc is a client library for the kludge remote logging server.
+package logsrvc
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+// Logger is value capable of sending logs to the log server.
+type Logger struct {
+	conn *net.TCPConn
+	node string
+}
+
+func Connect(node, addr string) (logger *Logger, err error) {
+	logger = new(Logger)
+
+	if addr != "" {
+		var logSrvAddr *net.TCPAddr
+		logSrvAddr, err = net.ResolveTCPAddr("tcp", addr)
+		if err != nil {
+			log.Printf("failed to resolve address %s: %s", addr,
+				err.Error())
+			return
+		}
+
+		logger.node = node
+		logger.conn, err = net.DialTCP("tcp", nil, logSrvAddr)
+		if err != nil {
+			log.Printf("couldn't connect to %s: %s", addr, err.Error())
+			return
+		}
+
+		logger.conn.SetKeepAlive(true)
+		log.SetOutput(logger.conn)
+	}
+	log.SetPrefix(logger.node + " ")
+	return
+}
+
+func (logger *Logger) Print(args ...interface{}) {
+	fmt.Println(args...)
+	log.Print(args...)
+}
+
+func (logger *Logger) Printf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+	fmt.Printf(format+"\n", args...)
+}
+
+func (logger *Logger) Println(args ...interface{}) {
+	fmt.Println(args...)
+	log.Println(args...)
+}
+
+func (logger *Logger) Shutdown() {
+	logger.conn.Close()
+}
---
+++ logsrv/logsrvc/client_test.go
@@ -0,0 +1,40 @@
+package logsrvc
+
+import (
+        "flag"
+	"fmt"
+	"testing"
+)
+
+var (
+        logSrv string
+        logger *Logger
+)
+
+func init() {
+        flAddr := flag.String("a", "127.0.0.1:5988", "address of log server")
+        flag.Parse()
+        logSrv = *flAddr
+}
+
+func TestConnect(t *testing.T) {
+	var err error
+
+	logger, err = Connect("test-client", logSrv)
+	if err != nil {
+		fmt.Printf("[!] error setting up test client: %s\n", err.Error())
+		t.FailNow()
+	}
+}
+
+func TestPrint(t *testing.T) {
+	logger.Print("hello, world")
+}
+
+func TestPrintf(t *testing.T) {
+	logger.Printf("testing log server %s", logSrv)
+}
+
+func TestShutdown(t *testing.T) {
+	logger.Shutdown()
+}
