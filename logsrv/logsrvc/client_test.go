bbb6276e7d88eb5685fee812ad47ebf1395387f7
Kyle Isom <coder@kyleisom.net>
2013-04-23 17:17:25 -0600

    update test to change client ID

--- logsrv/logsrvc/client_test.go
+++ logsrv/logsrvc/client_test.go
@@ -3,24 +3,34 @@ package logsrvc
 import (
 	"flag"
 	"fmt"
+        "github.com/gokyle/uuid"
+        "os"
 	"testing"
 )
 
 var (
 	logSrv string
 	logger *Logger
+        node   string
 )
 
 func init() {
 	flAddr := flag.String("address", "127.0.0.1:5988", "address of log server")
 	flag.Parse()
 	logSrv = *flAddr
+
+        var err error
+        node, err = uuid.GenerateV4String()
+        if err != nil {
+                fmt.Println("couldn't generate UUID:", err.Error())
+                os.Exit(1)
+        }
 }
 
 func TestConnect(t *testing.T) {
 	var err error
 
-	logger, err = Connect("test-client", logSrv)
+	logger, err = Connect(node, logSrv)
 	if err != nil {
 		fmt.Printf("[!] error setting up test client: %s\n", err.Error())
 		t.FailNow()
